6111ebc2d9c4c4b5c6faa55fdac9dc9a9173f7e8
Aito Nakajima <gmskazi@gmail.com>
2024-05-23 22:26:31 +0900

    next Count Letters

--- main.py
+++ main.py
@@ -1,17 +1,19 @@
 def main():
-    """Main Func"""
+    """Main bookbot Func"""
     book_path = "books/frankenstein.txt"
     text = get_book_text(book_path)
     number_of_words = get_num_words(text)
     print(f"{number_of_words} words found in the document")
 
 
-def get_book_text(path):
+def get_book_text(path) -> str:
+    """Return the book text."""
     with open(path) as f:
         return f.read()
 
 
-def get_num_words(words):
+def get_num_words(words) -> int:
+    """Return the number of words."""
     words = words.split()
     return len(words)
 
