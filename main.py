058cafcf9caa842c95807328f489dfddd0ae729c
Aito Nakajima <gmskazi@gmail.com>
2024-05-23 22:40:39 +0900

    Count Letters

--- main.py
+++ main.py
@@ -4,6 +4,7 @@ def main():
     text = get_book_text(book_path)
     number_of_words = get_num_words(text)
     print(f"{number_of_words} words found in the document")
+    print(count_characters(text))
 
 
 def get_book_text(path) -> str:
@@ -16,6 +17,18 @@ def get_num_words(words) -> int:
     """Return the number of words."""
     words = words.split()
     return len(words)
+
+
+def count_characters(texts) -> dict:
+    """Count the number of characters appear in the string."""
+    letters = {}
+    lowered_string = texts.lower()
+    for letter in lowered_string:
+        if letter in letters:
+            letters[letter] += 1
+        else:
+            letters[letter] = 1
+    return letters
 
 
 if __name__ == "__main__":
